611c70b15f15b57cc87c12ec3fe47cdbe11b17ff
Gustavo Perenciolo <26912712+perenciolo@users.noreply.github.com>
2021-02-04 15:48:38 -0300

    Update index.js

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -7,7 +7,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/test-auth', requireAuth, (req, res) => res.send('Hello PUTA!'));
+  app.get('/test-auth', requireAuth, (req, res) => res.send('Hello world!'));
   app.post('/signup', AuthController.signup);
   app.post('/signin', requireSignin, AuthController.signin);
 };
